71fb9152268a4f3f5cfbfc031ce84cf076101583
Wjrazzano89 <148407542+Wjrazzano89@users.noreply.github.com>
2023-11-08 18:01:36 -0500

    Added Arrays on script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
+let pokemonList = [
+    { name: Bulbasaur, height: 0.7, types: grass,poison },
+    { name: Squirtle, height: 0.5, types: water },
+    { name: Charmander, height: 0.6, types: fire },
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+];
