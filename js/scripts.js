040a86f762b455bd023790ff9ca244024f95cce0
Wjrazzano89 <148407542+Wjrazzano89@users.noreply.github.com>
2023-11-21 20:28:24 -0500

    Created new for loops, and updated app with names/heights of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 let pokemonList = [
-    { name: Bulbasaur, height: 0.7, types: grass,poison },
-    { name: Squirtle, height: 0.5, types: water },
-    { name: Charmander, height: 0.6, types: fire },
+    { name: "Bulbasaur", height: " (Height .7)", types: ["grass", "poison"] },
+    { name: "Squirtle", height: " (Height .5)", types: ["water"] },
+    { name: "Charmander", height: " (Height .6)", types: ["fire"] }
+]
 
-];
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < .7 && pokemonList[i].height > .5) {
+        console.log(pokemonList[i].name);
+        document.write(pokemonList[i].name + pokemonList[i].height)
+    } else if (pokemonList[i].height < .6) {
+        console.log(pokemonList[i].name);
+        document.write(pokemonList[i].name + pokemonList[i].height)
+    } else {
+        console.log(pokemonList[i].name);
+        document.write(pokemonList[i].name + pokemonList[i].height)
+    }
+}
