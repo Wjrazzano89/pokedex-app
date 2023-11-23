ce66b1c8623eb7aedbc8c9cb62c920f978e1c66b
Wjrazzano89 <148407542+Wjrazzano89@users.noreply.github.com>
2023-11-22 21:46:42 -0500

    Added conditional statement to else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 let pokemonList = [
-    { name: "Bulbasaur", height: " (Height .7)", types: ["grass", "poison"] },
-    { name: "Squirtle", height: " (Height .5)", types: ["water"] },
-    { name: "Charmander", height: " (Height .6)", types: ["fire"] }
+    { name: "Bulbasaur", height: " .7", types: ["grass", "poison"] },
+    { name: "Squirtle", height: " .5", types: ["water"] },
+    { name: "Charmander", height: " .6", types: ["fire"] }
 ]
 
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height < .7 && pokemonList[i].height > .5) {
         console.log(pokemonList[i].name);
-        document.write(pokemonList[i].name + pokemonList[i].height)
+        document.write(pokemonList[i].name + " - Height:" + pokemonList[i].height + "<br/>")
     } else if (pokemonList[i].height < .6) {
         console.log(pokemonList[i].name);
-        document.write(pokemonList[i].name + pokemonList[i].height)
+        document.write(pokemonList[i].name + " - Height:" + pokemonList[i].height + "<br/>" )
     } else {
         console.log(pokemonList[i].name);
-        document.write(pokemonList[i].name + pokemonList[i].height)
+        document.write(pokemonList[i].name + " - Height:" + pokemonList[i].height + " (Wow, that's a big Pokemon)" + "<br/>")
     }
 }
