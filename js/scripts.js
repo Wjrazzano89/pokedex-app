85097675579fc4532023ca41597865706e8dd50d
Wjrazzano89 <148407542+Wjrazzano89@users.noreply.github.com>
2023-12-04 18:42:38 -0500

    wrapped array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
+let pokemonRepository = (function () {
+        let pokemonList = [];
+      
+          function add(pokemon) {
+            pokemonList.push(pokemon);
+          }
+
+          function getAll() {
+            return pokemonList;
+          }
+
+          return {
+            add: add,
+            getAll: getAll
+          };
+      })();
+
+      console.log(pokemonRepository.getAll()); 
+      pokemonRepository.add({ name: 'Pikachu' });
+      console.log(pokemonRepository.getAll());
+
 let pokemonList = [
     { name: "Bulbasaur",
       height: " .7",
